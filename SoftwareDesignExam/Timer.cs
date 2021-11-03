b8359677e63598158a38f935d9c5dfac35eeaee6
ruben <rubencarlsen@gmail.com>
2021-11-03 13:54:52 +0100

    Timer implemented

--- SoftwareDesignExam/Timer.cs
+++ SoftwareDesignExam/Timer.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace ExamSoftwareDesign
+namespace SoftwareDesignExam
 {
-    class Timer
-    {
+    public sealed class Timer {
+        private static readonly Timer instance = new();
+        private Stopwatch _stopWatch = new();
+        
+        private Timer() {}
+
+        public void StartTimer() {
+            _stopWatch.Start();
+        }
+
+        public void StopTimer() {
+            _stopWatch.Stop();
+        }
+
+        public void TimesUp() {
+            _stopWatch.Reset();
+        }
+
+        public TimeSpan GetTime() {
+            return _stopWatch.Elapsed;
+        }
+
+        public static Timer Instance() {
+            return instance;
+        }
     }
+
 }
