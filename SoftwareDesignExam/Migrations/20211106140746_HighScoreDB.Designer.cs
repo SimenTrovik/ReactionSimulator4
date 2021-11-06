7a90a63787a25ac1c44f8cce5afde0f847164a17
Sakelig <71970061+Sakelig@users.noreply.github.com>
2021-11-06 17:49:41 +0100

    +Fixed methods to use IPlayer instead -lidl method needs replacement once getter for difficulty is made + can Save and Get player time from and to DB

--- SoftwareDesignExam/Migrations/20211106140746_HighScoreDB.Designer.cs
+++ SoftwareDesignExam/Migrations/20211106140746_HighScoreDB.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SoftwareDesignExam.ScoreDB;
 
 namespace SoftwareDesignExam.Migrations
 {
     [DbContext(typeof(ScoreContext))]
-    partial class ScoreContextModelSnapshot : ModelSnapshot
+    [Migration("20211106140746_HighScoreDB")]
+    partial class HighScoreDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Tests/ScoreDaoTest.cs
+++ Tests/ScoreDaoTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Media.Animation;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using SoftwareDesignExam;
 using SoftwareDesignExam.ScoreDB;
@@ -9,32 +11,54 @@ namespace Tests
 {
 	public class ScoreDaoTest
 	{
+
+		private ConcretePlayerFactory _factory;
+
 		[SetUp]
 		public void EmptyHighScoreTable()
 		{
 			using ScoreContext db = new();
 
+			//db.Database.Migrate(); lager Databasen når koden blir kjørt akkurat som "update-database" (tror jeg)
+
 			db.HighScores.RemoveRange(db.HighScores.ToList());
 
 			db.SaveChanges();
+
+			_factory = new ConcretePlayerFactory();
+
+
 		}
+
 
 		[Test]
 		public void ShouldUpdateScore()
 		{
-			ScoreDao.SaveScoreAndPlayer("Mario" , 64);
-			ScoreDao.SaveScore("Mario", 1);
 
-			int? mariosUpdatedScore = ScoreDao.GetScore("Mario");
+			var player = _factory.GetPlayer("Mario", PlayerFactory.PlayerType.Easy);
+			player.Score = 64;
+			ScoreDao.SaveScoreAndPlayer(player);
+
+			Assert.AreEqual(64, player.Score);
+
+			player.Score = 1;
+			ScoreDao.SaveScore(player);
+
+			int? mariosUpdatedScore = ScoreDao.GetScore(player);
 
 			Assert.AreEqual(1, mariosUpdatedScore);
 		}
 
+
 		[Test]
 		public void ShouldSaveScoreAndPlayer()
 		{
-			ScoreDao.SaveScoreAndPlayer("Halla", 1);
-			ScoreDao.SaveScoreAndPlayer("Balla", 5);
+			var player = _factory.GetPlayer("Halla", PlayerFactory.PlayerType.Normal);
+			var player2 = _factory.GetPlayer("Balla", PlayerFactory.PlayerType.Normal);
+
+
+			ScoreDao.SaveScoreAndPlayer(player);
+			ScoreDao.SaveScoreAndPlayer(player2);
 
 			using ScoreContext db = new();
 
@@ -46,25 +70,67 @@ namespace Tests
 			Assert.That(playerNames.Contains("Balla"));
 		}
 
+
 		[Test]
 		public void ShouldGivePlayerScore()
 		{
-			ScoreDao.SaveScoreAndPlayer("Doge", 420);
+			var player = _factory.GetPlayer("Doge", PlayerFactory.PlayerType.Normal);
+			player.Score = 420;
 
-			Assert.AreEqual(420, ScoreDao.GetScore("Doge"));
+			ScoreDao.SaveScoreAndPlayer(player);
+
+			Assert.AreEqual(420, ScoreDao.GetScore(player));
 		}
+
 
 		[Test]
 		public void ShouldGiveListBasedOnHighestScore()
 		{
-			ScoreDao.SaveScoreAndPlayer("Forsen" , 3);
-			ScoreDao.SaveScoreAndPlayer("Asmongold" , 5);
-			ScoreDao.SaveScoreAndPlayer("Tyler1" , 1);
+			var player1 = _factory.GetPlayer("Forsen", PlayerFactory.PlayerType.Easy);
+			player1.Score = 3;
+
+			var player2 = _factory.GetPlayer("Asmongold", PlayerFactory.PlayerType.Normal);
+			player2.Score = 5;
+			
+			var player3 = _factory.GetPlayer("Tyler1", PlayerFactory.PlayerType.Normal);
+			player3.Score = 1;
+
+			ScoreDao.SaveScoreAndPlayer(player1);
+			ScoreDao.SaveScoreAndPlayer(player2);
+			ScoreDao.SaveScoreAndPlayer(player3);
 
 			Assert.AreEqual("Asmongold", ScoreDao.GetHighScores()[0]);
 			Assert.AreEqual("Tyler1", ScoreDao.GetHighScores()[2]);
 		}
 
+
+		[Test]
+		public void ShouldGivePlayerDifficulty()
+		{
+
+			var player = _factory.GetPlayer("Pepe", PlayerFactory.PlayerType.Easy);
+
+			ScoreDao.SaveScoreAndPlayer(player);
+
+			PlayerFactory.PlayerType playerDifficulty = ScoreDao.GetPlayerDifficulty(player);
+
+			Assert.AreEqual(PlayerFactory.PlayerType.Easy, playerDifficulty);
+		}
+
+
+		[Test]
+		public void ShouldUpdateAndGetPlayerTime()
+		{
+			var player = _factory.GetPlayer("TimeWizard", PlayerFactory.PlayerType.Normal);
+
+			ScoreDao.SaveScoreAndPlayer(player);
+
+			ScoreDao.SavePlayerTime(player, 777);
+
+			Assert.AreEqual(777, ScoreDao.GetPlayerTime(player));
+
+
+		}
 
 	}
 }
