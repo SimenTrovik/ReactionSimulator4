080002fd5db2db3f9eb5fd315810f85366422509
Dontosh <me@torsteinstrand.com>
2021-11-08 12:41:10 +0100

    Added WPF-windows

---
+++ SoftwareDesignExam/Migrations/20211104125113_ScoreDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoftwareDesignExam.Migrations
+{
+    public partial class ScoreDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
