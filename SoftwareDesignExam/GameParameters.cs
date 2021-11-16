86e28042c68325a779ccc8964c3a214643bc7284
ruben <rubencarlsen@gmail.com>
2021-11-16 21:17:00 +0100

    Updated GameParameters to include the timer's variables

--- SoftwareDesignExam/GameParameters.cs
+++ SoftwareDesignExam/GameParameters.cs
@@ -8,6 +8,11 @@ namespace SoftwareDesignExam
 {
     public static class GameParameters
     {
+        public static int TimesUpTime { get; } = 3000;      // Milliseconds before timer stops and a game is over
+        public static int LeastRandomVal { get; } = 2000;   // Least    amount of milliseconds before the light turns green
+        public static int MaxRandomVal { get; } = 5000;     // Maximum  amount of milliseconds before the light turns green
+
+        // Multipliers for calculating player scores
         public static Dictionary<PlayerType, int> multipliers = new() {
             { PlayerType.Easy, 8 },
             { PlayerType.Normal, 10 }
