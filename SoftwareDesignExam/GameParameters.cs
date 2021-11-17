37e091f9ca4df84bb5b2a08e61299746176e9764
Rubstubs <61133459+Rubstubs@users.noreply.github.com>
2021-11-17 13:44:49 +0100

    refactored (#36)

--- SoftwareDesignExam/GameParameters.cs
+++ SoftwareDesignExam/GameParameters.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace SoftwareDesignExam
 {
     public static class GameParameters
     {
-        public static int TimesUpTime { get; } = 1000;      // Milliseconds before timer stops and a game is over
-        public static int LeastRandomVal { get; } = 2000;   // Least    amount of milliseconds before the light turns green
-        public static int MaxRandomVal { get; } = 5000;     // Maximum  amount of milliseconds before the light turns green
+        public static int ReactionDeadline { get; } = 1000;     // Milliseconds before timer stops and a game is over
+        public static int StartTimeMinimum { get; } = 2000;     // Minimum amount of milliseconds before the light turns green
+        public static int StartTimeMaximum { get; } = 5000;     // Maximum amount of milliseconds before the light turns green
+        readonly private static double EasyPlayerMultiplyer = 1.1;
+        readonly private static double NormalPlayerMultiplyer = 1;
 
         // Multipliers for calculating player scores
         public static Dictionary<PlayerType, double> multipliers = new() {
-            { PlayerType.Easy, 1.1 },
-            { PlayerType.Normal, 1 }
+            { PlayerType.Easy, EasyPlayerMultiplyer },
+            { PlayerType.Normal, NormalPlayerMultiplyer }
         };
 
         public static double CalculateScore(PlayerType playerType, int timeInMs)
         {
+            // Calculations for player scores
+            if (Timer.Instance().WaitingToStart) return 0;
             return 1000 - timeInMs / multipliers[playerType];
         }
     }
